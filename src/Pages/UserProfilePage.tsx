1de713b729316a0f76ff93475878ce1c120640a4
shubhampal <65231036+akashhxthakur@users.noreply.github.com>
2023-08-24 16:24:11 +0530

    format files

--- src/Pages/UserProfilePage.tsx
+++ src/Pages/UserProfilePage.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const UserProfilePage = () => {
-  return (
-    <div>UserProfilePage</div>
-  )
+  return <div>UserProfilePage</div>
 }
 
 export default UserProfilePage
