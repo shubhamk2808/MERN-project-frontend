acc4d8cd24a0cc2383c29b45fb078480b5531d12
shubhampal <shubhamk2808@gmail.com>
2023-08-24 18:07:02 +0530

    added theme changes

--- src/MuiTheme/theme.ts
+++ src/MuiTheme/theme.ts
@@ -1,19 +1,91 @@
-import { red } from '@mui/material/colors'
-import { createTheme } from '@mui/material/styles'
+import { createTheme } from "@mui/material/styles";
 
-// Create a theme instance.
-const theme = createTheme({
+interface CustomPalette {
+  primary: string;
+  text: string;
+  textSecondary: string;
+  background: string;
+  backgroundVariant: string;
+  border: string;
+  borderLight: string;
+}
+
+const lightThemeProperties: CustomPalette = {
+  primary: 'rgba(215,113,88,1)',
+  text: 'rgba(58,52,51,1)',
+  textSecondary: 'rgba(58,52,51,0.7)',
+  background: 'rgba(255,255,255,1)',
+  backgroundVariant: 'rgba(251,249,249,1)',
+  border: 'rgba(58,52,51,0.12)',
+  borderLight: 'rgba(58,52,51,0.05)',
+};
+
+const darkThemeProperties: CustomPalette = {
+  primary: 'rgba(220,120,95,1)',
+  text: 'rgba(241,233,231,1)',
+  textSecondary: 'rgba(241,233,231,0.6)',
+  background: 'rgba(0,0,0,1)',
+  backgroundVariant: 'rgba(28,26,26,1)',
+  border: 'rgba(241,233,231,0.15)',
+  borderLight: 'rgba(241,233,231,0.05)',
+};
+
+export const lightTheme = createTheme({
   palette: {
+    mode: "light",
     primary: {
-      main: '#556cd6',
+      main: lightThemeProperties.primary,
     },
-    secondary: {
-      main: '#19857b',
+    text: {
+      primary: lightThemeProperties.text,
+      secondary: lightThemeProperties.textSecondary,
     },
-    error: {
-      main: red.A400,
+    background: {
+      default: lightThemeProperties.background,
+      paper: lightThemeProperties.backgroundVariant,
+    },
+    action: {
+      active: lightThemeProperties.primary,
+      hover: lightThemeProperties.primary,
     },
   },
-})
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          backgroundColor: lightThemeProperties.backgroundVariant,
+        },
+      },
+    },
+  },
+});
 
-export default theme
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: darkThemeProperties.primary,
+    },
+    text: {
+      primary: darkThemeProperties.text,
+      secondary: darkThemeProperties.textSecondary,
+    },
+    background: {
+      default: darkThemeProperties.background,
+      paper: darkThemeProperties.backgroundVariant,
+    },
+    action: {
+      active: darkThemeProperties.primary,
+      hover: darkThemeProperties.primary,
+    },
+  },
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          backgroundColor: darkThemeProperties.backgroundVariant,
+        },
+      },
+    },
+  },
+});
--- src/Redux/Reducers/index.tsx
+++ src/Redux/Reducers/index.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import counterReducer from './CounterReducer'
+import themeSlice from './themeSlice'
 
 const rootReducer = combineReducers({
   counter: counterReducer,
+  theme: themeSlice,
   // ...add other reducers here
 })
 
