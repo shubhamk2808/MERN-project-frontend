fde555959d626763ef756a2b845072a151e61596
shubhampal <shubhamk2808@gmail.com>
2023-08-25 18:25:10 +0530

    checking api calls

--- src/Redux/Actions/UserAction.tsx
+++ src/Redux/Actions/UserAction.tsx
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
+import ApiInstance from 'src/Services/Axios';
 
 export const loginUser: any = createAsyncThunk(
   'auth/loginUser',
@@ -9,3 +10,28 @@ export const loginUser: any = createAsyncThunk(
     }
   }
 )
+
+export const getProducts = createAsyncThunk('user/products',
+  async (payload, { rejectWithValue }) => {
+    console.log({ payload }, "check products")
+    try {
+      const response = await ApiInstance.get('/products');
+      console.log({ response })
+      return response
+    } catch (err: any) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+)
+
+export const getUsers = async () => {
+  console.log("get users");
+  try {
+    const response = await ApiInstance.get('/users');
+    const token = response.data.token; // Assuming your server returns a token
+    localStorage.setItem('token', token);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
